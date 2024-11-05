a66b5b076a9dfa8a5073d30ae04d0f78b6532ec2
Devlin <devlincodes@gmail.com>
2024-11-05 13:44:13 -0800

    add restart and quit buttons

--- scripts/main.gd
+++ scripts/main.gd
@@ -9,8 +9,12 @@ extends ColorRect
 
 
 func _ready() -> void:
-	left_score_zone.player_scored.connect(_on_player_scored.bind(2))
-	right_score_zone.player_scored.connect(_on_player_scored.bind(1))
+	if not left_score_zone.player_scored.is_connected(_on_player_scored.bind(2)):
+		left_score_zone.player_scored.connect(_on_player_scored.bind(2))
+	if not right_score_zone.player_scored.is_connected(_on_player_scored.bind(1)):
+		right_score_zone.player_scored.connect(_on_player_scored.bind(1))
+	if not pause_menu.game_restart.is_connected(restart):
+		pause_menu.game_restart.connect(restart)
 	start_game()
 
 
@@ -41,3 +45,22 @@ func _on_player_scored(player: int) -> void:
 			goal_screen.visible = true
 
 	get_tree().create_timer(3).timeout.connect(start_game)
+
+
+func restart() -> void:
+	player_1_score.text = str(0)
+	player_2_score.text = str(0)
+	goal_screen.visible = false
+	pause_menu.visible = false
+
+	for child in get_children():
+		if child is Ball:
+			child.queue_free()
+
+	const BALL := preload("res://scenes/ball.tscn")
+	var new_ball = BALL.instantiate()
+
+	add_child(new_ball)
+	new_ball.respawn()
+	new_ball.move()
+	get_tree().paused = false
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,8 +1,31 @@
 extends PanelContainer
-#
-#
+
+
+signal game_restart
+
+
+@onready var restart: Button = %Restart
+@onready var quit: Button = %Quit
+
+
+func _ready() -> void:
+	if not restart.pressed.is_connected(_on_restart_pressed):
+		restart.pressed.connect(_on_restart_pressed)
+	if not quit.pressed.is_connected(_on_quit_pressed):
+		quit.pressed.connect(_on_quit_pressed)
+
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("player1_pause") or Input.is_action_just_pressed("player2_pause"):
 		visible = true if visible == false else false
 
 		get_tree().paused = true if visible == true else false
+
+
+
+func _on_restart_pressed() -> void:
+	game_restart.emit()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
