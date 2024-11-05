191fe198ce26c3c4a7408291d9e1259ddc790fae
Devlin <devlincodes@gmail.com>
2024-11-05 00:52:42 -0800

    adjust speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,23 +5,23 @@ extends CharacterBody2D
 
 
 enum Type {player1, player2}
-const SPEED = 4
+const SPEED = 320
 
 
 func _physics_process(delta: float) -> void:
-	player1_controls() if Player == Type.player1 else player2_controls()
+	player1_controls(delta) if Player == Type.player1 else player2_controls(delta)
 	move_and_slide()
 
 
-func player1_controls() -> void:
+func player1_controls(delta: float) -> void:
 	if Input.is_action_pressed("player1_up"):
-		position.y -= SPEED
+		position.y -= SPEED * delta
 	if Input.is_action_pressed("player1_down"):
-		position.y += SPEED
+		position.y += SPEED * delta
 
 
-func player2_controls() -> void:
+func player2_controls(delta: float) -> void:
 	if Input.is_action_pressed("player2_up"):
-		position.y -= SPEED
+		position.y -= SPEED * delta
 	if Input.is_action_pressed("player2_down"):
-		position.y += SPEED
+		position.y += SPEED * delta
