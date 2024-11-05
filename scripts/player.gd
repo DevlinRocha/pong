1232f6dfc63f18066fa349c432f8bd9a408589f1
Devlin <devlincodes@gmail.com>
2024-11-05 00:55:43 -0800

    refactor speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,7 +8,7 @@ enum Type {player1, player2}
 const SPEED = 320
 
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	player1_controls(delta) if Player == Type.player1 else player2_controls(delta)
 	move_and_slide()
 
